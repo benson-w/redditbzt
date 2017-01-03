a5e8f364e29d82de2f72796ab40713b0f815e0c0
benson-w <yuwang15@illinois.edu>
2017-01-02 22:38:09 -0600

    bot reading reddit posts

---
+++ read.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+# will only work if python binary is in /usr/bin
+
+import praw 
+
+reddit = praw.Reddit('bot1')
+
+subreddit = reddit.subreddit("dota2")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("-------\n")
